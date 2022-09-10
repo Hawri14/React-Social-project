dced87f40e82370e3d523422a9bc23924a6538bd
Hawri <hawrikazaz@gmail.com>
2022-09-10 20:32:36 +0100

    added posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,6 +10,16 @@ export const getPost =async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+//this means that the post has been successful
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).jason({ message: error.message }); //means that it was not successful
+    }
 }
