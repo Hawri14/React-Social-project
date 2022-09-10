8f6fade92bc77d04a4437f735580d1b2c6f82bed
Hawri <hawrikazaz@gmail.com>
2022-09-10 19:19:45 +0100

    swapped posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import { getPost } from '../controllers/posts.js'
+
 const router = express.Router();
 //this is reached by going to loachlost:500/posts since we added a prefix of 'posts' to all rounts here
 
-
-router.get('/', (req, res) => {
-    res.send('THIS WORKS!!!');
-});
+router.get('/', getPost);
 
 export default router;
