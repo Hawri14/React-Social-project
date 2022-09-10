272ea05245d4168c6fe41e53ae7be322cf3d3b40
Hawri <hawrikazaz@gmail.com>
2022-09-10 20:23:27 +0100

    created posts and array

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPost } from '../controllers/posts.js'
+import { getPost, createPost } from '../controllers/posts.js'
 
 const router = express.Router();
 //this is reached by going to loachlost:500/posts since we added a prefix of 'posts' to all rounts here
 
 router.get('/', getPost);
+router.post('/', createPost);
 
 export default router;
